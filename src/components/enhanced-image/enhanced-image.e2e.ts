81a3c36e7dba4d20d498b5869fb53a9ccdd47792
Jan Cavel <caveljan@gmail.com>
2019-03-12 10:32:07 +0200

    setup: test

--- src/components/enhanced-image/enhanced-image.e2e.ts
+++ src/components/enhanced-image/enhanced-image.e2e.ts
@@ -10,17 +10,4 @@ describe('enhanced-image', () => {
         const element = await page.find('enhanced-image');
         expect(element).toHaveClass('hydrated');
     });
-
-    it('renders changes to the name data', async () => {
-        const page = await newE2EPage();
-
-        await page.setContent('<enhanced-image></enhanced-image>');
-        const component = await page.find('enhanced-image');
-        const element = await page.find('enhanced-image >>> div');
-        expect(element.textContent).toEqual(``);
-
-        component.setProperty('src', 'test');
-        await page.waitForChanges();
-        expect(element.textContent).toEqual(`test`);
-    });
 });
