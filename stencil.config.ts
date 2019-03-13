00799457f866f61f0fd8b37796f3ef1665d0472a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-03-13 08:49:01 +0200

    setup: copy test-assets

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,12 +4,15 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
     namespace: 'text-select-image-html',
+    copy: [
+        { src: 'test-assets' }
+    ],
     outputTargets:[
         { type: 'dist' },
         { type: 'docs' },
         {
             type: 'www',
-            serviceWorker: null // disable service workers
+            serviceWorker: null
         }
     ]
 };
