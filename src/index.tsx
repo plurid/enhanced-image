438ff02f543c518198fa514635ad9350962e7432
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-03-22 14:22:29 +0200

    setup: typescript

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
+
+// import 'enhanced-image-html/enhanced-image-html';
+
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root') as HTMLElement
+);
+
+// registerServiceWorker();
