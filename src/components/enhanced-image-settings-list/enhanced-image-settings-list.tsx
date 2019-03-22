0f4be3c8472147955753a7001412cc4d14c1262b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-03-22 10:51:47 +0200

    dev: toggle defaults

--- src/components/enhanced-image-settings-list/enhanced-image-settings-list.tsx
+++ src/components/enhanced-image-settings-list/enhanced-image-settings-list.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State } from '@stencil/core';
 
 import {
     SLIDER_DEFAULTS,
@@ -62,6 +62,15 @@ export class EnhancedImageSettingsList {
 
     @Prop() noAbout: boolean;
 
+    @State() toggledDefaults: boolean = false;
+    @State() previousValues = {
+        invert: this.colorsInverted,
+        contrast: SLIDER_DEFAULTS.contrast,
+        hue: SLIDER_DEFAULTS.hue,
+        saturation: SLIDER_DEFAULTS.saturation,
+        brightness: SLIDER_DEFAULTS.brightness,
+    };
+
     setSliderDefaults = () => {
         this.setSlider('contrast', SLIDER_DEFAULTS.contrast);
         this.setSlider('hue', SLIDER_DEFAULTS.hue);
@@ -74,11 +83,43 @@ export class EnhancedImageSettingsList {
         this.saveButton.download = imageName;
     }
 
+    toggleDefaults = () => {
+        this.toggledDefaults = !this.toggledDefaults;
+        if(this.toggledDefaults) {
+            this.previousValues = {
+                invert: this.colorsInverted,
+                contrast: this.contrastSliderValue,
+                hue: this.hueSliderValue,
+                saturation: this.saturationSliderValue,
+                brightness: this.brightnessSliderValue,
+            }
+            this.resetToDefaults();
+        } else {
+            if (this.previousValues.invert) {
+                this.colorsInvert();
+            }
+            this.setSlider('contrast', this.previousValues.contrast);
+            this.setSlider('hue', this.previousValues.hue);
+            this.setSlider('saturation', this.previousValues.saturation);
+            this.setSlider('brightness', this.previousValues.brightness);
+        }
+    }
+
     resetToDefaults = () => {
         if (this.colorsInverted) {
             this.colorsInvert();
         }
         this.setSliderDefaults();
+
+        if (
+            this.colorsInverted === !!SLIDER_DEFAULTS.invert
+            && this.contrastSliderValue === SLIDER_DEFAULTS.contrast
+            && this.hueSliderValue === SLIDER_DEFAULTS.hue
+            && this.saturationSliderValue === SLIDER_DEFAULTS.saturation
+            && this.brightnessSliderValue === SLIDER_DEFAULTS.brightness
+        ) {
+            this.toggledDefaults = false;
+        }
     }
 
     aboutEnhancedImage = () => {
@@ -87,8 +128,6 @@ export class EnhancedImageSettingsList {
     }
 
     render() {
-        console.log(this.textSelect);
-
         return (
             <div class={`enhanced-image-settings-list enhanced-image-settings-list-${this.location}`}>
                 <ul>
@@ -133,9 +172,9 @@ export class EnhancedImageSettingsList {
 
                     <li>
                         <enhanced-image-button-checkmark
-                            toggle={this.colorsInvert}
+                            toggle={this.toggleDefaults}
                             text={'Toggle Defaults'}
-                            checked={this.colorsInverted}
+                            checked={this.toggledDefaults}
                         />
                     </li>
 
