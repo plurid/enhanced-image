5dc3d3a41924a28067df8fd64f8e1587086de819
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-04-13 14:47:55 +0300

    enhanced-image-html > dev: icon

--- packages/enhanced-image-html/src/components/enhanced-image-settings-button/enhanced-image-settings-button.tsx
+++ packages/enhanced-image-html/src/components/enhanced-image-settings-button/enhanced-image-settings-button.tsx
@@ -1,7 +1,8 @@
 import { Component, Prop } from '@stencil/core';
 
 import settingsIcon from '../../assets/settings-icon.svg';
-import settingsTextIcon from '../../assets/settings-text-icon.svg';
+// import settingsTextIcon from '../../assets/settings-text-icon.svg';
+import settingsTextColoredFlatIcon from '../../assets/settings-text-colored-flat-icon.svg';
 
 
 
@@ -19,7 +20,7 @@ export class EnhancedImageSettings {
 
     render() {
         const innerHTMLIcon = this.icon === 'textselect'
-            ? settingsTextIcon
+            ? settingsTextColoredFlatIcon
             : settingsIcon;
 
         return (
@@ -27,7 +28,15 @@ export class EnhancedImageSettings {
                 onClick={this.toggle}
                 class="enhanced-image-settings-button"
                 innerHTML={innerHTMLIcon}
-            />
+            >
+                <svg style="width:0;height:0;position:absolute;" aria-hidden="true" focusable="false">
+                    <linearGradient id="enhanced-image-settings-button-gradient" x1="0" y1="0" x2="1" y2="1">
+                        <stop offset="0%" stop-color="hsl(310, 50%, 45%)" />
+                        <stop offset="40%" stop-color="hsl(325, 70%, 35%)" />
+                        <stop offset="100%" stop-color="hsl(285, 60%, 20%)" />
+                    </linearGradient>
+                </svg>
+            </div>
         );
     }
 }
