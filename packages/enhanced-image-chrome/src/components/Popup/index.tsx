8ac147a46e1ec6f80140d0aae0bcb56d5eb745fc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-04-14 22:31:49 +0300

    enhanced-image-chrome > dev: popup structure

--- packages/enhanced-image-chrome/src/components/Popup/index.tsx
+++ packages/enhanced-image-chrome/src/components/Popup/index.tsx
@@ -1,31 +1,98 @@
 import * as React from 'react';
 
+import './styles.css';
 
 
-const popupStyle: any = {
-    cursor: 'pointer',
-    padding: 20,
-    textAlign: 'center',
-    userSelect: 'none',
-    width: 240,
-}
 
 class Popup extends React.Component {
+    public state = {
+        extensionOn: true,
+        loggedIn: true,
+    }
+
     public openOptions = () => {
-        // chrome.runtime.openOptionsPage();
+        chrome.runtime.openOptionsPage();
+    }
+
+    public openImages = () => {
+        console.log('openImagesRepo');
+    }
+
+    public toggleExtension = () => {
+        this.setState({
+            extensionOn: !this.state.extensionOn
+        });
     }
 
     public render() {
         return (
-            <div style={ {...popupStyle} }>
-                Enhanced Image
+            <ul className="popup">
+                <li className="onOff">
+                    <div>
+                        Enhanced Image is
+                    </div>
+                    <div className="onOffButtons">
+                        <span
+                            className="button onOffButton"
+                            onClick={this.toggleExtension}
+                        >
+                            {this.state.extensionOn ? 'on' : 'off'}
+                        </span>
+                    </div>
+                </li>
 
-                <div
-                    onClick={this.openOptions}
-                >
-                    Options
-                </div>
-            </div>
+                <li className="logged">
+                    {this.state.loggedIn && (
+                        <>
+                            <div className="loggedUser">
+                                <div>Logged in as</div>
+                                <div className="username">caveljan</div>
+                            </div>
+                            <div className="button logoutButton">
+                                Logout
+                            </div>
+                        </>
+                    )}
+
+                    {!this.state.loggedIn && (
+                        <div>
+                            <span className="button">
+                                Login
+                            </span>
+                        </div>
+                    )}
+                </li>
+
+                {this.state.loggedIn && (
+                    <li className="available">
+                        <ul>
+                            <li>1,624 text extractions</li>
+                            <li>567 generates</li>
+                            <li>984 transviews</li>
+                        </ul>
+                    </li>
+                )}
+
+                {this.state.loggedIn && (
+                    <li>
+                        <span
+                            className="button buttonImages"
+                            onClick={this.openImages}
+                        >
+                            Images
+                        </span>
+                    </li>
+                )}
+
+                <li>
+                    <span
+                        className="button buttonOptions"
+                        onClick={this.openOptions}
+                    >
+                        Options
+                    </span>
+                </li>
+            </ul>
         );
     }
 }
