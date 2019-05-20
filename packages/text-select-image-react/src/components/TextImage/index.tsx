a078558df5425804630d5c0d207d1f524508bf14
Jan Cavel <caveljan@gmail.com>
2019-05-20 15:59:01 +0300

    text-select-image-react > dev: TextImage cleanup

--- packages/text-select-image-react/src/components/TextImage/index.tsx
+++ packages/text-select-image-react/src/components/TextImage/index.tsx
@@ -59,62 +59,24 @@ class TextImage extends Component<
         xCoord: this.props.text.xCoord,
         yCoord: this.props.text.yCoord,
 
-        // textEditable: false,
-        // draggable: false,
-        // dragging: false,
-        // pos1: 0,
-        // pos2: 0,
-        // pos3: 0,
-        // pos4: 0,
-        // fontSizeValue: 0,
-        // letterSpacingValue: 0,
-        // wordSpacingValue: 0,
-        // fontFamilyValue: '',
-        // colorValue: '',
-        // colorValueStyle: '',
+        textEditable: false,
+        draggable: false,
+        dragging: false,
 
-        // textBold: false,
-        // textItalic: false,
-        // textContent: '',
-        // textChanged: false,
-        // textViewable: false,
+        pos1: 0,
+        pos2: 0,
+        pos3: 0,
+        pos4: 0,
+
         // editorXCoord: 0,
         // editorYCoord: 0,
-        // toggleEditor: false,
-        // editable: false,
     };
 
     public componentDidLoad() {
-        // const {
-        //     text,
-        //     xCoord,
-        //     yCoord,
-        // } = this.state;
-
-        // this.setState({
-        //     xCoord,
-        //     yCoord,
-        //     fontSizeValue: text.fontSize,
-        //     fontFamilyValue: text.fontFamily,
-        //     letterSpacingValue: text.letterSpacing,
-        //     wordSpacingValue: text.wordSpacing,
-        //     textLink: text.link,
-        //     textLinkToValue: text.linkTo,
-        //     textBold: text.bold,
-        //     textItalic: text.italic,
-        //     textViewable: text.viewable,
-        // });
-
-        // this.colorValue = this.text.color || this.colorValue;
-        // if (this.editable) {
-        //     this.colorValue = this.text.color || 'black';
-        //     this.colorValueStyle = this.colorValue;
-        // }
     }
 
     public componentWillUpdate() {
         // if (this.draggable) {
-        //     // console.log('aa', this.textImageSpanContent);
         //     this.textImageSpanContent.onmousedown = this.dragMouseDown;
         //     this.textImageSpanContent.onmouseup = this.mouseUp;
         // } else {
@@ -136,52 +98,19 @@ class TextImage extends Component<
         // this.editorYCoord = this.textImageSpan.offsetTop < 34
         //     ? this.textImageSpan.offsetHeight
         //     : -34;
-
-        // this.colorValueStyle = !this.editable ? '' : this.colorValue;
-
-        // if (this.recordChanged()) {
-        //     const record = { ...this.text };
-        //     record.xCoord = this.xCoord;
-        //     record.yCoord = this.yCoord;
-        //     record.fontSize = this.fontSizeValue;
-        //     record.letterSpacing = this.letterSpacingValue;
-        //     record.wordSpacing = this.wordSpacingValue;
-        //     record.fontFamily = this.fontFamilyValue;
-        //     record.color = this.colorValue;
-        //     record.bold = this.textBold;
-        //     record.italic = this.textItalic;
-        //     record.content = this.textImageSpanContent.innerText;
-
-        //     this.updateText(this.text.id, record);
-        // }
     }
 
     public render() {
         const {
-            textLink,
-            textLinkToValue,
             text,
             showEditor,
 
             yCoord,
             xCoord,
-            // editable,
-            // draggable,
-            // dragging,
-            // textViewable,
-            // colorValueStyle,
-            // fontFamilyValue,
-            // fontSizeValue,
-            // textBold,
-            // textItalic,
-            // letterSpacingValue,
-            // wordSpacingValue,
 
-
-
-
+            draggable,
+            dragging,
         } = this.state;
-
 
         const {
             color,
@@ -203,29 +132,25 @@ class TextImage extends Component<
         } = this.context;
 
         // console.log(text);
-
         const textContent = link
-            ? (<a href={linkTo} target="_blank">
-                {text.content}
-            </a>)
-            : (
-                (<div>
+            ? (
+                <a href={linkTo} target="_blank">
                     {text.content}
-                </div>)
+                </a>
+            )
+            : (
+                <div>
+                    {text.content}
+                </div>
             );
 
         return (
             <div>
                <StyledTextImage
                     editMode={toggledEditable}
-
-                    // className={`
-                    //     text-image-span
-                    //     ${editable ? 'text-image-span-editable' : '' }
-                    //     ${draggable ? 'text-image-span-draggable' : '' }
-                    //     ${dragging ? 'text-image-span-dragging' : '' }
-                    //     ${textViewable ? 'text-image-span-viewable' : '' }
-                    // `}
+                    dragMode={draggable}
+                    draggingMode={dragging}
+                    viewable={viewable}
 
                     style={{
                         top: yCoord + 'px',
@@ -244,155 +169,74 @@ class TextImage extends Component<
                     onMouseLeave={this.showEditor}
                 >
                     <div
-                        // className="text-image-span-content"
-                        // contentEditable={textEditable}
                         // onKeyUp={updateTextContent}
                     >
                         {textContent}
                     </div>
 
-                    {/* {showEditor && (
+                    {showEditor && (
                         <TextImageEditor
                             toggleElement={this.toggleElement}
                             changeValue={this.changeValue}
                             text={text}
                         />
-                    )} */}
-                    <TextImageEditor
+                    )}
+                    {/* <TextImageEditor
                         toggleElement={this.toggleElement}
                         changeValue={this.changeValue}
                         text={text}
-                    />
+                    /> */}
                 </StyledTextImage>
             </div>
         );
     }
 
-    // public render() {
-    //     const { text } = this.state;
+    private dragMouseDown = (e: any) => {
+        this.setState({
+            dragging: true,
+        });
 
-    //     return (
-    //         <span
-    //             class={`
-    //                 text-image-span
-    //                 ${this.editable ? 'text-image-span-editable' : '' }
-    //                 ${this.draggable ? 'text-image-span-draggable' : '' }
-    //                 ${this.dragging ? 'text-image-span-dragging' : '' }
-    //                 ${this.textViewable ? 'text-image-span-viewable' : '' }
-    //             `}
-    //             style={{
-    //                 top: this.yCoord + 'px',
-    //                 left: this.xCoord + 'px',
-    //                 color: this.colorValueStyle,
-    //                 fontFamily: this.fontFamilyValue,
-    //                 fontSize: this.fontSizeValue + 'px',
-    //                 fontWeight: this.textBold ? 'bold' : 'normal',
-    //                 fontStyle: this.textItalic ? 'italic' : 'normal',
-    //                 letterSpacing: this.letterSpacingValue + 'px',
-    //                 // lineHeight: text.lineHeight + '',
-    //                 wordSpacing: this.wordSpacingValue + 'px',
-    //             }}
-    //             onMouseEnter={this.toggleEditor}
-    //             onMouseLeave={this.toggleEditor}
-    //             ref={(el) => this.textImageSpan = el as HTMLSpanElement}
-    //         >
-    //             <span
-    //                 class="text-image-span-content"
-    //                 ref={(el) => this.textImageSpanContent = el as HTMLSpanElement}
-    //                 contentEditable={this.textEditable}
-    //                 onKeyUp={this.updateTextContent}
-    //             >
-    //                 {this.textLink
-    //                     ? (<a href={this.textLinkToValue} target="_blank">
-    //                         {text.content}
-    //                     </a>)
-    //                     : (<span>
-    //                         {text.content}
-    //                     </span>)
-    //                 }
-    //             </span>
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
 
-    //             {this.showEditor && (
-    //                 <span
-    //                     style={{
-    //                         left: `${this.editorXCoord}px`,
-    //                         top: `${this.editorYCoord}px`,
-    //                     }}
-    //                     class="text-image-span-editor"
-    //                 >
-    //                     <TextImageEditor
-    //                         textEditable={this.textEditable}
-    //                         toggleTextEditable={this.toggleTextEditable}
-    //                         draggable={this.draggable}
-    //                         toggleDraggable={this.toggleDraggable}
-    //                         toggleEditor={this.toggleEditor}
-    //                         textViewable={this.textViewable}
-    //                         toggleTextViewable={this.toggleTextViewable}
+        this.setState({
+            pos3: e.clientX,
+            pos4: e.clientY,
+        });
 
-    //                         textId={this.text.id}
-    //                         fontSizeValue={this.fontSizeValue}
-    //                         letterSpacingValue={this.letterSpacingValue}
-    //                         wordSpacingValue={this.wordSpacingValue}
-    //                         fontFamilyValue={this.fontFamilyValue}
-    //                         colorValue={this.colorValue}
-    //                         textLink={this.textLink}
-    //                         textLinkToValue={this.textLinkToValue}
-    //                         textBold={this.textBold}
-    //                         textItalic={this.textItalic}
+        document.onmouseup = this.closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = this.elementDrag;
+    }
 
-    //                         changeValue={this.changeValue}
-    //                         toggleElement={this.toggleElement}
+    private elementDrag = (e: any) => {
+        e.preventDefault();
 
-    //                         selectableFonts={selectableFonts}
+        const { pos3, pos4 } = this.state;
 
-    //                         duplicateText={this.duplicateText}
-    //                         removeText={this.removeText}
-    //                     />
-    //                 </span>
-    //             )}
-    //         </span>
-    //     );
-    // }
+        // calculate the new cursor position:
+        this.setState({
+            pos1: pos3,
+            pos2: pos4,
+            pos3: e.clientX,
+            pos4: e.clientY,
+            // xCoord: this.textImageSpan.offsetLeft - pos1,
+            // yCoord = this.textImageSpan.offsetTop - pos2,
+        })
+    }
 
+    private closeDragElement = () => {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 
-    // private dragMouseDown = (e: any) => {
-    //     this.dragging = true;
-
-    //     e = e || window.event;
-    //     e.preventDefault();
-    //     // get the mouse cursor position at startup:
-    //     this.pos3 = e.clientX;
-    //     this.pos4 = e.clientY;
-
-    //     document.onmouseup = this.closeDragElement;
-    //     // call a function whenever the cursor moves:
-    //     document.onmousemove = this.elementDrag;
-    // }
-
-    // private elementDrag = (e: any) => {
-    //     e.preventDefault();
-
-    //     // calculate the new cursor position:
-    //     this.pos1 = this.pos3 - e.clientX;
-    //     this.pos2 = this.pos4 - e.clientY;
-    //     this.pos3 = e.clientX;
-    //     this.pos4 = e.clientY;
-
-    //     this.xCoord = this.textImageSpan.offsetLeft - this.pos1;
-    //     this.yCoord = this.textImageSpan.offsetTop - this.pos2;
-    // }
-
-    // private closeDragElement = () => {
-    //     /* stop moving when mouse button is released:*/
-    //     document.onmouseup = null;
-    //     document.onmousemove = null;
-    // }
-
-    // private mouseUp = () => {
-    //     this.setState({
-    //         dragging: false,
-    //     });
-    // }
+    private mouseUp = () => {
+        this.setState({
+            dragging: false,
+        });
+    }
 
     private showEditor = () => {
         const { toggledEditable } = this.context;
@@ -404,39 +248,39 @@ class TextImage extends Component<
         }
     }
 
-    // private toggleDraggable = () => {
-    //     this.setState((prevState: any) => ({
-    //         draggable: !prevState.draggable,
-    //     }));
+    private toggleDraggable = () => {
+        this.setState((prevState: any) => ({
+            draggable: !prevState.draggable,
+        }));
 
-    //     const { textEditable } = this.state;
+        const { textEditable } = this.state;
 
-    //     if (textEditable) {
-    //         this.setState({
-    //             textEditable: false,
-    //         });
-    //     }
-    // }
+        if (textEditable) {
+            this.setState({
+                textEditable: false,
+            });
+        }
+    }
 
-    // private toggleTextEditable = () => {
-    //     this.setState((prevState: any) => ({
-    //         textEditable: !prevState.textEditable,
-    //     }));
+    private toggleTextEditable = () => {
+        this.setState((prevState: any) => ({
+            textEditable: !prevState.textEditable,
+        }));
 
-    //     const { draggable } = this.state;
+        const { draggable } = this.state;
 
-    //     if (draggable) {
-    //         this.setState({
-    //             draggable: false,
-    //         });
-    //     }
-    // }
+        if (draggable) {
+            this.setState({
+                draggable: false,
+            });
+        }
+    }
 
-    // private toggleTextViewable = () => {
-    //     this.setState((prevState: any) => ({
-    //         textViewable: !prevState.textViewable,
-    //     }));
-    // }
+    private toggleTextViewable = () => {
+        this.setState((prevState: any) => ({
+            textViewable: !prevState.textViewable,
+        }));
+    }
 
     private changeValue = (type: string, value: number | string) => {
         const typeValue = `${type}Value`;
@@ -447,30 +291,10 @@ class TextImage extends Component<
         // this[element] = !this[element];
     }
 
-    // private updateTextContent = () => {
+    private updateTextContent = () => {
     //     this.textContent = this.textImageSpanContent.innerText;
     //     this.textChanged = true;
-    // }
-
-    // private recordChanged = () => {
-    //     const text = this.text;
-    //     if (
-    //         text.xCoord !== this.xCoord
-    //         || text.yCoord !== this.yCoord
-    //         || text.fontSize !== this.fontSizeValue
-    //         || text.letterSpacing !== this.letterSpacingValue
-    //         || text.wordSpacing !== this.wordSpacingValue
-    //         || text.fontFamily !== this.fontFamilyValue
-    //         || text.color !== this.colorValue
-    //         || text.bold !== this.textBold
-    //         || text.italic !== this.textItalic
-    //         || text.viewable !== this.textViewable
-    //         // || this.textChanged
-    //     ) {
-    //         return true;
-    //     }
-    //     return false;
-    // }
+    }
 }
 
 
