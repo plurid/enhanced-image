926be97fefe56cd9d86dc10ed9e71e5a1c5efc18
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-05-27 22:21:39 +0300

    enhanced-image-react > dev: save/share image

---
+++ packages/enhanced-image-react/src/utils/image.ts
@@ -0,0 +1,26 @@
+export const loadImage = (url: string) => {
+    return new Promise(
+        (response) => {
+            const image = new Image();
+            image.onload = (() => response(image));
+            image.src = url;
+        },
+    );
+};
+
+
+/**
+ * Edited from
+ * https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob#Polyfill
+ */
+export const dataURIToBlob = (dataURI: string) => {
+    const binStr = atob(dataURI.split(',')[1]);
+    const len = binStr.length;
+    const arr = new Uint8Array(len);
+
+    for (let i = 0; i < len; i++) {
+        arr[i] = binStr.charCodeAt(i);
+    }
+
+    return new Blob([arr]);
+};
