b77fdf9d5b604c52a87501ed78e5390fe0847a85
Jan Cavel <caveljan@gmail.com>
2019-05-28 08:20:38 +0300

    text-select-image-react > dev: embedded use

--- packages/text-select-image-react/src/data/constants.ts
+++ packages/text-select-image-react/src/data/constants.ts
@@ -8,4 +8,4 @@ export const DEFAULT_THEME = 'gradient';
 export const ABOUT_URL = 'https://github.com/plurid/text-select-image';
 
 
-export const PLURID_API = 'https://api.plurid.com/graphql';
+export const PLURID_API = 'https://api.plurid.dev/graphql';
