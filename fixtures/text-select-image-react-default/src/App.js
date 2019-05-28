d5e4ed83c016a260eef6eace68ee9527f699946a
Jan Cavel <caveljan@gmail.com>
2019-05-28 16:09:36 +0300

    text-select-image-react > dev: TextSelectImage

--- fixtures/text-select-image-react-default/src/App.js
+++ fixtures/text-select-image-react-default/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 // import foodImage from './assets/food-vegetables-text.jpg';
-import populationDensityEurope from './assets/population-density-europe.png';
+// import populationDensityEurope from './assets/population-density-europe.png';
 // import populationDensityEuropeB from './assets/population-density-europe-b.png';
 // import webP from './assets/89d1516a7cf15e086f3cd605a606ce6df78ecc4c3899499cc95a7dca9e30fb4c.webp';
-// import image from './assets/image.jpg';
+import image from './assets/image.jpg';
 
 import TextSelectImage from '@plurid/text-select-image-react';
 
@@ -19,7 +19,7 @@ class App extends Component {
                 >
                     <TextSelectImage
                         theme="night"
-                        src={populationDensityEurope}
+                        src={image}
                         controls={true}
                         apiEndpoint="http://localhost:3360/graphql"
                     />
