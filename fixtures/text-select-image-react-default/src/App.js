c891c4513565d04bdb296d1c1b4373a03ee8f267
Jan Cavel <caveljan@gmail.com>
2019-06-12 21:52:39 +0300

    text-select-image-react > dev: TextSelectImage

--- fixtures/text-select-image-react-default/src/App.js
+++ fixtures/text-select-image-react-default/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         return (
             <div className="app">
                 <div
-                    style={ {width: 800, margin: '150px auto'} }
+                    style={ {width: 900, margin: '150px auto'} }
                 >
                     <TextSelectImage
                         theme="night"
@@ -25,7 +25,8 @@ class App extends Component {
                         // src={image}
                         controls={true}
                         // getTextOnLoad={true}
-                        apiEndpoint="http://192.168.1.2:3360/graphql"
+                        apiEndpoint="http://192.168.1.3:3360/graphql"
+                        // apiEndpoint="http://localhost:3360/graphql"
                         // apiEndpoint="https://api.plurid.com/graphql"
                     />
                 </div>
