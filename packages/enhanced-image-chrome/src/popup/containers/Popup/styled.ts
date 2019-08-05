bad15e8642952592473592a3eeed8869b604df48
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-05 23:19:44 +0300

    enhanced-image-chrome > dev: popup

--- packages/enhanced-image-chrome/src/popup/containers/Popup/styled.ts
+++ packages/enhanced-image-chrome/src/popup/containers/Popup/styled.ts
@@ -13,12 +13,35 @@ export const StyledPopup: any = styled.div`
     height: 200px;
     width: 250px;
     text-align: left;
+    user-select: none;
+
+     a {
+        color: ${(props: any) => {
+            return props.theme.colorSecondary;
+        }};
+        text-decoration: none;
+     }
 `;
 
 
 export const StyledPopupContainer: any = styled.div`
     height: 100%;
-    width: 100%;
+    width: 200px;
     display: grid;
-    place-content: center;
+    align-items: center;
+    margin: 0 auto;
+`;
+
+
+export const StyledOptionsItemLeftRight = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 5px 0;
+
+    svg {
+        width: 14px;
+        height: 14px;
+        margin: 5px;
+        fill: white;
+    }
 `;
