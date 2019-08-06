51a6e05cb6ba2c6f0f4b67d095b768175c14c970
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-06 09:57:52 +0300

    enhanced-image-chrome > dev: LoginView

---
+++ packages/enhanced-image-chrome/src/components/InputText/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {
+    StyledInputText,
+} from './styled';
+
+
+
+interface InputTextProps {
+    theme: any;
+    value: string;
+    atChange: any;
+    placeholder?: string;
+    password?: boolean;
+}
+
+
+const InputText: React.FC<InputTextProps> = (props) => {
+    const {
+        theme,
+        value,
+        atChange,
+        password,
+        placeholder,
+    } = props;
+
+    return (
+        <StyledInputText
+            theme={theme}
+        >
+            <input
+                type={password ? 'password' : 'text'}
+                value={value}
+                onChange={atChange}
+                placeholder={placeholder ? placeholder : ''}
+            />
+        </StyledInputText>
+    );
+}
+
+
+export default InputText;
