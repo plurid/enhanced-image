6db7b268f4b6d1b6d9b2bc43f1bd54cd90efde00
Jan Cavel <caveljan@gmail.com>
2019-08-06 09:49:06 +0300

    enhanced-image-chrome > dev: LoginView

---
+++ packages/enhanced-image-chrome/src/components/CreateAccountButton/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import {
+    StyledCreateAccountButton,
+} from './styled';
+
+import ButtonInline from '../ButtonInline';
+
+import ExternalLinkIcon from '../../assets/buttons/external-link-icon';
+
+
+
+interface CreateAccountButtonProps {
+    theme: any;
+}
+
+const PLURID_ACCOUNT_DOMAIN = 'https://account.plurid.com';
+
+const CreateAccountButton: React.FC<CreateAccountButtonProps> = (props) => {
+    const {
+        theme,
+    } = props;
+
+    return (
+        <StyledCreateAccountButton>
+            <a
+                href={PLURID_ACCOUNT_DOMAIN}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <ButtonInline
+                    atClick={() => {}}
+                    theme={theme}
+                    styles={{display: 'flex', alignItems: 'center'}}
+                >
+                    <div>
+                        create account
+                    </div>
+
+                    <div
+                        style={{display: 'flex', alignItems: 'center'}}
+                    >
+                        {ExternalLinkIcon}
+                    </div>
+                </ButtonInline>
+            </a>
+        </StyledCreateAccountButton>
+    );
+}
+
+
+export default CreateAccountButton;
---
+++ packages/enhanced-image-chrome/src/components/LoginView/styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+
+
+export const StyledLoginView: any = styled.div`
+`;
