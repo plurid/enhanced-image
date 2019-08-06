e04cc6988e590dcf6723ab665eb7b248c875a201
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-06 16:21:15 +0300

    enhanced-image-chrome > dev: LoggedInView, ItemActivate

---
+++ packages/enhanced-image-chrome/src/components/ItemActivate/index.tsx
@@ -0,0 +1,66 @@
+import React, {
+    useState,
+} from 'react';
+
+import {
+    StyledItemLoggedInAs,
+} from './styled';
+
+import ButtonInline from '../ButtonInline';
+
+import { PLURID_ACCOUNT_DOMAIN } from '../../constants';
+
+
+
+interface ItemLoggedInAsProps {
+    theme: any;
+    active: boolean;
+    type: string;
+}
+
+
+const ItemLoggedInAs: React.FC<ItemLoggedInAsProps> = (props) => {
+    const [mouseOver, setMouseOver] = useState(false);
+
+    const {
+        theme,
+        active,
+        type,
+    } = props;
+
+    return (
+        <StyledItemLoggedInAs
+            theme={theme}
+            onMouseEnter={() => setMouseOver(true)}
+            onMouseLeave={() => setMouseOver(false)}
+            onMouseMove={() => {!mouseOver ? setMouseOver(true) : null}}
+        >
+            <div>
+                {type}
+            </div>
+
+            {(!mouseOver || active)
+            ? (
+                <div>
+                    {active ? 'active' : 'not active'}
+                </div>
+            ) : (
+                <a
+                    href={PLURID_ACCOUNT_DOMAIN}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <ButtonInline
+                        theme={theme}
+                        atClick={() => {}}
+                    >
+                        activate
+                    </ButtonInline>
+                </a>
+            )}
+        </StyledItemLoggedInAs>
+    );
+}
+
+
+export default ItemLoggedInAs;
--- packages/enhanced-image-chrome/src/constants/index.ts
+++ packages/enhanced-image-chrome/src/constants/index.ts
@@ -5,3 +5,6 @@ import environment from '../environment';
 export const API_URI = environment.production
     ? 'https://api.plurid.com/graphql'
     : 'http://localhost:33600/graphql';
+
+
+export const PLURID_ACCOUNT_DOMAIN = 'https://account.plurid.com/depict';
