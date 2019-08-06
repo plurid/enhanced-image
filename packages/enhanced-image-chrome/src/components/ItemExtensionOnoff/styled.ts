447ed370fd7b93fbba2cb48ea8faff4f9fb4ee44
Jan Cavel <caveljan@gmail.com>
2019-08-06 15:55:41 +0300

    enhanced-image-chrome > dev: popup, items

---
+++ packages/enhanced-image-chrome/src/components/ItemExtensionOnoff/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+
+
+export const StyledItemExtensionOnOff: any = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 5px 0;
+    /* margin-top: 20px; */
+`;
---
+++ packages/enhanced-image-chrome/src/components/ItemNotLoggedIn/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+
+
+export const StyledItemNotLoggedIn: any = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 5px 0;
+`;
