314161b76fb653151815bde7f0821f62d172681b
Jan Cavel <caveljan@gmail.com>
2019-08-06 10:29:05 +0300

    enhanced-image-chrome > dev: LoginView

---
+++ packages/enhanced-image-chrome/src/components/Button/styled.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+
+
+export const StyledButton: any = styled.div`
+    button {
+        background-color: ${(props: any) => {
+            return props.theme.backgroundColorSecondary;
+        }};
+        color: ${(props: any) => {
+            return props.theme.colorPrimary;
+        }};
+        cursor: pointer;
+        user-select: none;
+        box-shadow: 0px 5px 5px 0px ${(props: any) => {
+            return props.theme.shadow;
+        }};
+        border: none;
+        outline: none;
+        padding: 0;
+        text-align: center;
+        font-size: 14px;
+        width: 100%;
+        height: 30px;
+        border-radius: 30px;
+        margin-bottom: 10px;
+        transition: box-shadow 100ms linear;
+    }
+
+    button:active {
+        box-shadow: 0px 2px 2px 0px ${(props: any) => {
+            return props.theme.shadow;
+        }};
+    }
+
+    button:disabled {
+        opacity: 0.5;
+        box-shadow: 0px 5px 5px 0px ${(props: any) => {
+            return props.theme.shadow;
+        }};
+        cursor: not-allowed;
+    }
+`;
--- packages/enhanced-image-chrome/src/components/InputText/styled.ts
+++ packages/enhanced-image-chrome/src/components/InputText/styled.ts
@@ -3,5 +3,15 @@ import styled from 'styled-components';
 
 
 export const StyledInputText = styled.div`
-
+    input {
+        background: transparent;
+        color: white;
+        border: none;
+        outline: none;
+        padding: 0;
+        text-align: center;
+        font-size: 14px;
+        width: 100%;
+        height: 30px;
+    }
 `;
