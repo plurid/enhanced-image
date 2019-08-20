fb1fc5d7cf0dd6e173aa433abfa3d00cf73f2a39
Jan Cavel <caveljan@gmail.com>
2019-08-20 11:01:56 +0300

    text-select-image-react > dev: TextSelectImage, fixture

--- fixtures/text-select-image-react-default/src/App.js
+++ fixtures/text-select-image-react-default/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-// import foodImage from './assets/food-vegetables-text.jpg';
+import foodImage from './assets/food-vegetables-text.jpg';
 // import catsImage from './assets/cats.jpg';
-import imagePurpose from './assets/image-purpose.jpg';
+// import imagePurpose from './assets/image-purpose.jpg';
 // import img from './assets/img.png';
 // import img2 from './assets/img2.jpg';
 // import imageBlack from './assets/image-black.jpg';
@@ -20,14 +20,16 @@ class App extends Component {
         return (
             <div className="app">
                 <div
-                    style={ {width: 900, margin: '150px auto'} }
+                    style={ {width: 900, height: 500, margin: '150px auto'} }
                 >
                     <TextSelectImage
                         theme="depict"
-                        src={imagePurpose}
+                        src={foodImage}
+                        // src={imagePurpose}
                         controls={true}
                         // getTextOnLoad={true}
-                        depictImageID="b08fb57bbaf34157bffaf14147ba7efbf12a00e9b48e4ee4953abfe1d463dd66"
+                        // depictImageID="b08fb57bbaf34157bffaf14147ba7efbf12a00e9b48e4ee4953abfe1d463dd66"
+                        depictImageID="891a3dce43a24665a6cc6ac5b7b48bf5b6ab38ab7fb54303ad03f94b31b18a32"
                         apiEndpoint="http://192.168.1.3:33600/graphql"
                         // apiEndpoint="http://localhost:33600/graphql"
                         // apiEndpoint="https://api.plurid.com/graphql"
---
+++ packages/text-select-image-react/src/utils/graphql.ts
@@ -0,0 +1,21 @@
+export const deleteTypenames = (data: any) => {
+    let dataObject = null;
+    if (Array.isArray(data)) {
+        dataObject = [ ...data ];
+    } else {
+        dataObject = { ...data };
+    }
+
+    for (const property in dataObject) {
+        if (property === '__typename') {
+            delete dataObject[property];
+        }
+
+        if (typeof dataObject[property] === 'object' && dataObject[property] !== null) {
+            const dataObjectProperty = deleteTypenames(dataObject[property]);
+            dataObject[property] = dataObjectProperty;
+        }
+    }
+
+    return dataObject;
+}
