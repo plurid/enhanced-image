e41e4ea7552edda706aac9307340ca54de049e1c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-23 11:13:58 +0300

    text-select-image-react > dev: graphql

--- packages/text-select-image-react/src/graphql/client.ts
+++ packages/text-select-image-react/src/graphql/client.ts
@@ -12,15 +12,15 @@ const apolloClient = (uri: string) => {
             onError(({ graphQLErrors, networkError }) => {
                 if (graphQLErrors)
                     graphQLErrors.map(({ message, locations, path }) =>
-                        console.log(
-                            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-                        ),
+                    console.log(
+                        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+                    ),
                     );
                 if (networkError) console.log(`[Network error]: ${networkError}`);
             }),
             new HttpLink({
                 uri,
-            }),
+            })
         ]),
         cache: new InMemoryCache(),
     });
--- packages/text-select-image-react/src/graphql/query.ts
+++ packages/text-select-image-react/src/graphql/query.ts
@@ -1,60 +1,62 @@
 import gql from 'graphql-tag';
 
-import {
-    TextSelectImage_DepictImageDataFragment,
-} from './fragments';
 
 
-
-export const GET_DEPICT_IMAGE_DATA_BY_URL_WITH_API_KEY = gql`
-    query GetDepictImageDataByURLWithApiKey($imageURL: String!, $apiKey: String!) {
-        getDepictImageDataByURLWithApiKey(imageURL: $imageURL, apiKey: $apiKey) {
+export const getTextSelectImage = gql`
+    query TextSelectImage($imageSha: String!) {
+        textSelectImage(imageSha: $imageSha) {
             status
             errors {
                 path
                 message
-                type
             }
-            depictImageData {
-                ...TextSelectImage_DepictImageDataFragment
+            textSelectImage {
+                user {
+                    username
+                }
+                imagePath
+                imageSource
+                imageHeight
+                imageWidth
+                imageText {
+                    id
+                    currentVersionId
+                    versions {
+                        id
+                        createdBy
+                        xCoordPercentage
+                        yCoordPercentage
+                        perspective
+                        rotation
+                        skew
+                        color
+                        fontFamily
+                        fontSizePercentage
+                        bold
+                        italic
+                        letterSpacingPercentage
+                        lineHeight
+                        wordSpacingPercentage
+                        content
+                        link
+                        linkTo
+                        viewable
+                    }
+                }
             }
         }
     }
-    ${TextSelectImage_DepictImageDataFragment}
-`;
+`
 
 
-export const GET_DEPICT_IMAGE_DATA_BY_URL_WITH_USER_TOKEN = gql`
-    query GetDepictImageDataByURLWithUserToken($imageURL: String!, $userToken: String!) {
-        getDepictImageDataByURLWithUserToken(imageURL: $imageURL, userToken: $userToken) {
+export const extractTextSelectImage = gql`
+    query ExtractTextSelectImage($imageSrc: String!, $imageSha: String!) {
+        extractTextSelectImage(imageSrc: $imageSrc, imageSha: $imageSha) {
             status
             errors {
                 path
                 message
-                type
-            }
-            depictImageData {
-                ...TextSelectImage_DepictImageDataFragment
             }
         }
     }
-    ${TextSelectImage_DepictImageDataFragment}
-`;
-
-
-export const GET_DEPICT_IMAGE_DATA_BY_IMAGE_ID = gql`
-    query GetDepictImageDataByImageID($imageID: String!) {
-        getDepictImageDataByImageID(imageID: $imageID) {
-            status
-            errors {
-                path
-                message
-                type
-            }
-            depictImageData {
-                ...TextSelectImage_DepictImageDataFragment
-            }
-        }
-    }
-    ${TextSelectImage_DepictImageDataFragment}
 `;
