aacff463648f419cf327a2ed245b632cfbbcd8d4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-30 10:44:00 +0300

    setup: packages

--- text/packages/text-select-image-react/src/test.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import TextSelectImage from './';
-
-
-
-it('renders without crashing', () => {
-    shallow(<TextSelectImage />);
-});
