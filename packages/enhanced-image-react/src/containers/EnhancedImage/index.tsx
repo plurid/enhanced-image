c747ac6b819d696f7ca13ca2536ec7015622f244
Jan Cavel <caveljan@gmail.com>
2019-09-30 12:15:19 +0300

    enhanced-image-react > setup: package

--- packages/enhanced-image-react/src/containers/EnhancedImage/index.tsx
+++ packages/enhanced-image-react/src/containers/EnhancedImage/index.tsx
@@ -2,18 +2,40 @@ import React from 'react';
 
 import {
     StyledEnhancedImage,
+    StyledImageContainer,
 } from './styled';
 
 import {
     EnhancedImageProperties,
 } from '../../data/interfaces';
 
+import themes from '@plurid/utilities.themes';
 
 
-const EnhancedImage: React.FC<EnhancedImageProperties> = () => {
+
+const EnhancedImage: React.FC<EnhancedImageProperties> = (properties) => {
+    const {
+        src,
+        alt,
+
+        imageStyle,
+    } = properties;
+
+    const theme = themes.plurid;
+
     return (
-        <StyledEnhancedImage>
-            enhanced image
+        <StyledEnhancedImage
+            theme={theme}
+        >
+            <StyledImageContainer>
+                <img
+                    src={src}
+                    alt={alt || 'Image'}
+                    // onLoad={this.handleLoadedImage}
+                    style={{...imageStyle}}
+                    // data-depict={true}
+                />
+            </StyledImageContainer>
         </StyledEnhancedImage>
     );
 }
