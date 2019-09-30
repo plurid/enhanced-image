e5518383b47ca57f66411620b6028ffd1e471db0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-30 13:56:18 +0300

    enhanced-image-react > setup: package

--- packages/enhanced-image-react/src/containers/EnhancedImage/index.tsx
+++ packages/enhanced-image-react/src/containers/EnhancedImage/index.tsx
@@ -1,42 +1,90 @@
 import React from 'react';
 
+import './styles.css';
 import {
     StyledEnhancedImage,
     StyledImageContainer,
 } from './styled';
 
+import Context from '../../services/utilities/context';
+
 import {
     EnhancedImageProperties,
+    Context as IContext,
 } from '../../data/interfaces';
 
-import themes from '@plurid/utilities.themes';
+import {
+    PLURID_API_ENDPOINT,
+} from '../../data/constants';
+
+import themes, { Theme } from '@plurid/utilities.themes';
 
 
 
 const EnhancedImage: React.FC<EnhancedImageProperties> = (properties) => {
     const {
         src,
+        srcset,
         alt,
 
+        theme,
+        height,
+        about,
+
         imageStyle,
+
+        atLoad,
+
+        apiEndpoint,
+
+        apiKey,
+        userToken,
+        depictImageID,
     } = properties;
 
-    const theme = themes.plurid;
+    const _theme: Theme = theme && themes[theme]
+        ? themes[theme]
+        : themes.plurid;
+    const _alt = alt || '';
+    const _height = height || 500;
+    const _about = about === undefined ? true : about;
+
+    const _apiEndpoint = apiEndpoint ? apiEndpoint : PLURID_API_ENDPOINT;
+
+    const context: IContext = {
+        src,
+        srcset,
+        alt: _alt,
+
+        theme: _theme,
+        height: _height,
+        about: _about,
+
+        apiEndpoint: _apiEndpoint,
+
+        apiKey,
+        userToken,
+        depictImageID,
+    };
 
     return (
-        <StyledEnhancedImage
-            theme={theme}
+        <Context.Provider
+            value={context}
         >
-            <StyledImageContainer>
-                <img
-                    src={src}
-                    alt={alt || 'Image'}
-                    // onLoad={this.handleLoadedImage}
-                    style={{...imageStyle}}
-                    // data-depict={true}
-                />
-            </StyledImageContainer>
-        </StyledEnhancedImage>
+            <StyledEnhancedImage
+                theme={_theme}
+            >
+                <StyledImageContainer>
+                    <img
+                        src={src}
+                        alt={alt || 'Image'}
+                        // onLoad={this.handleLoadedImage}
+                        style={{...imageStyle}}
+                        // data-depict={true}
+                    />
+                </StyledImageContainer>
+            </StyledEnhancedImage>
+        </Context.Provider>
     );
 }
 
--- packages/enhanced-image-react/src/data/constants/index.ts
+++ packages/enhanced-image-react/src/data/constants/index.ts
@@ -1,11 +1,17 @@
+import environment from '../../services/utilities/environment';
+
+
 export const UPDATE_DEBOUNCE = 5000;
 
 export const DEFAULT_THEME = 'gradient';
 
 export const ABOUT_URL = 'https://github.com/plurid/enhanced-image';
 
-export const PLURID_API = 'https://api.plurid.com/graphql';
-
+const PLURID_API_ENDPOINT_DEV = 'https://api.plurid.dev/graphql';
+const PLURID_API_ENDPOINT_PROD = 'https://api.plurid.com/graphql';
+export const PLURID_API_ENDPOINT = environment.production
+    ? PLURID_API_ENDPOINT_PROD
+    : PLURID_API_ENDPOINT_DEV;
 
 
 export const SLIDER_NAMES = {
