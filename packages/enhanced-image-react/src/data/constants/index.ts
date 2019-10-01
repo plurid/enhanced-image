99fd21014bf7d8efc19c460cbc890d2f45ba8daf
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-10-01 16:48:15 +0300

    enhanced-image-react > dev: EnhancedImage, graphql client

--- packages/enhanced-image-react/src/data/constants/index.ts
+++ packages/enhanced-image-react/src/data/constants/index.ts
@@ -3,22 +3,24 @@ import environment from '../../services/utilities/environment';
 
 export const UPDATE_DEBOUNCE = 5000;
 
-export const DEFAULT_THEME = 'gradient';
+export const DEFAULT_THEME = 'plurid';
 
 export const ABOUT_URL = 'https://github.com/plurid/enhanced-image';
 
-const PLURID_API_ENDPOINT_DEV = 'https://api.plurid.dev/graphql';
 const PLURID_API_ENDPOINT_PROD = 'https://api.plurid.com/graphql';
+const PLURID_API_ENDPOINT_DEV = 'https://api.plurid.dev/graphql';
+const PLURID_API_ENDPOINT_LOCAL = 'http://localhost:33600/playground';
 export const PLURID_API_ENDPOINT = environment.production
     ? PLURID_API_ENDPOINT_PROD
-    : PLURID_API_ENDPOINT_DEV;
+    : environment.local
+        ? PLURID_API_ENDPOINT_LOCAL
+        : PLURID_API_ENDPOINT_DEV;
 
-const DEPICT_DOMAIN_DEV = 'https://depict.plurid.dev';
 const DEPICT_DOMAIN_PROD = 'https://depict.plurid.com';
+const DEPICT_DOMAIN_DEV = 'https://depict.plurid.dev';
 export const DEPICT_DOMAIN = environment.production
     ? DEPICT_DOMAIN_PROD
     : DEPICT_DOMAIN_DEV;
-
 
 
 export const SETTINGS_MENU_HEIGHT_DIFFERENCE = 70;
--- packages/enhanced-image-react/src/services/utilities/environment.ts
+++ packages/enhanced-image-react/src/services/utilities/environment.ts
@@ -1,4 +1,5 @@
 const environment = {
+    local: process.env.MODE_ENV === 'local',
     development: process.env.MODE_ENV === 'development',
     production: process.env.MODE_ENV === 'production',
 };
