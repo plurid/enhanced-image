72c3c7d71b14205d8401e8707ee0c1bc3c93bec8
Jan Cavel <caveljan@gmail.com>
2019-10-31 15:19:45 +0200

    enhanced-image-react > setup: package

--- packages/enhanced-image-react/src/services/graphql/client/index.ts
+++ packages/enhanced-image-react/src/services/graphql/client/index.ts
@@ -1,8 +1,29 @@
-import ApolloClient from 'apollo-boost';
+// import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
 
-const client = (uri: string) => new ApolloClient({ uri });
+// const client = (uri: string) => new ApolloClient({
+//     uri,
+//     credentials: 'include',
+// });
+
+const client = (uri: string) => {
+    const cache = new InMemoryCache();
+    const link = new HttpLink({
+        uri,
+        credentials: 'include',
+    });
+
+    const client = new ApolloClient({
+        cache,
+        link,
+    });
+
+    return client;
+}
 
 
 export default client;
