7b8357a108d536a0f6ef6005cb3489eb87800f7d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-11-03 14:01:29 +0200

    enhanced-image-react > dev: assets

--- packages/enhanced-image-react/src/assets/icons/settings-menu/background-color-white.tsx
+++ packages/enhanced-image-react/src/assets/icons/settings-menu/background-color-white.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const SVG = (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
-        <title>White</title>
+        <title>White Background</title>
 
         <g>
             <path d="M500,50C251.47,50,50,251.47,50,500S251.47,950,500,950,950,748.53,950,500,748.53,50,500,50Zm0,803c-195,0-353-158-353-353S305,147,500,147,853,305,853,500,695,853,500,853Z"/>
