2190c71f322aa6b13b20f2c9db19e8352575261d
Jan Cavel <caveljan@gmail.com>
2019-11-14 10:36:50 +0200

    enhanced-image-chrome > dev: contentscript

--- packages/enhanced-image-chrome/source/programs/contentscript/index.tsx
+++ packages/enhanced-image-chrome/source/programs/contentscript/index.tsx
@@ -120,55 +120,59 @@ const isImage = (location: string) => {
 }
 
 async function contentscript() {
-    const href = location.href;
+    try {
+        const href = location.href;
 
-    const { extensionOn } = await chromeStorage.get('extensionOn');
+        const { extensionOn } = await chromeStorage.get('extensionOn');
 
-    if (!extensionOn) {
-        return;
-    }
+        if (!extensionOn) {
+            return;
+        }
 
-    if (isImage(href)) {
-        const imagesArray = Array.from(document.images);
+        if (isImage(href)) {
+            const imagesArray = Array.from(document.images);
 
-        for (let i = 0; i < imagesArray.length; i++) {
-            const image = imagesArray[i];
-            if (image) {
-                if (image.getAttribute('data-depict')) {
-                    return;
+            for (let i = 0; i < imagesArray.length; i++) {
+                const image = imagesArray[i];
+                if (image) {
+                    if (image.getAttribute('data-depict')) {
+                        return;
+                    }
+
+                    const height = image.offsetHeight;
+                    const width = image.offsetWidth;
+                    const src = image.src;
+                    const alt = image.alt;
+
+                    document.body.removeChild(image);
+
+                    const rootId = `root-enhanced-image-${i}`;
+                    const root = document.createElement('div');
+                    root.id = rootId;
+                    root.style.cssText = `margin: auto; height: ${height}px; width: ${width}px;`;
+                    document.body.appendChild(root);
+
+                    const { theme } = await chromeStorage.get('theme');
+                    const { token } = await chromeStorage.get('token');
+                    const { refreshToken } = await chromeStorage.get('refreshToken');
+                    const { options } = await chromeStorage.get('options');
+
+                    ReactDOM.render(
+                        <Image
+                            src={src}
+                            alt={alt}
+                            theme={theme}
+                            userToken={token}
+                            refreshToken={refreshToken}
+                            options={options}
+                        />,
+                        document.getElementById(rootId) as HTMLElement,
+                    );
                 }
-
-                const height = image.offsetHeight;
-                const width = image.offsetWidth;
-                const src = image.src;
-                const alt = image.alt;
-
-                document.body.removeChild(image);
-
-                const rootId = `root-enhanced-image-${i}`;
-                const root = document.createElement('div');
-                root.id = rootId;
-                root.style.cssText = `margin: auto; height: ${height}px; width: ${width}px;`;
-                document.body.appendChild(root);
-
-                const { theme } = await chromeStorage.get('theme');
-                const { token } = await chromeStorage.get('token');
-                const { refreshToken } = await chromeStorage.get('refreshToken');
-                const { options } = await chromeStorage.get('options');
-
-                ReactDOM.render(
-                    <Image
-                        src={src}
-                        alt={alt}
-                        theme={theme}
-                        userToken={token}
-                        refreshToken={refreshToken}
-                        options={options}
-                    />,
-                    document.getElementById(rootId) as HTMLElement,
-                );
             }
         }
+    } catch (error) {
+        console.log(error);
     }
 }
 
