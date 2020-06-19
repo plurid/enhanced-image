979f1cb90360b460864ab8d32264a469e307bad6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-19 22:25:10 +0300

    enhanced-image-react > setup: package

---
+++ packages/enhanced-image-react/source/containers/EnhancedImage/__stories__/index.stories.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+// import themes from '@plurid/plurid-themes';
+
+import { storiesOf } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+
+import {
+    withKnobs,
+    // boolean,
+    // number,
+    // select,
+    // text,
+} from '@storybook/addon-knobs';
+
+import EnhancedImage from '../';
+
+import foodText from '../../../../test/assets/food-text.jpg';
+
+
+
+// const actions = {
+//     // atClick: action('atClick'),
+// };
+
+// const themeLabel = 'Theme';
+// const computeThemeOptions = () => {
+//     const options: any = {};
+//     for (const key of Object.keys(themes)) {
+//         options[key] = key;
+//     }
+//     return options;
+// }
+// const themeOptions = computeThemeOptions();
+// const defaultThemeValue = 'plurid';
+
+storiesOf(
+    'EnhancedImage',
+    module,
+)
+.addDecorator(withKnobs)
+.add('basic', () => {
+    // const theme = select(themeLabel, themeOptions, defaultThemeValue);
+
+    return (
+        <div
+            style={{
+                padding: '4rem',
+            }}
+        >
+            <EnhancedImage
+                src={foodText}
+                // {...actions}
+                // theme={themes[theme]}
+            />
+        </div>
+    );
+});
--- packages/enhanced-image-react/source/typings.d.ts
+++ packages/enhanced-image-react/source/typings.d.ts
@@ -2,3 +2,33 @@ declare module '*.css' {
     const content: { [className: string]: string };
     export default content;
 }
+
+declare module '*.jpeg' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.jpg' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.png' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.webp' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.gif' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.svg' {
+    const value: any;
+    export default value;
+}
