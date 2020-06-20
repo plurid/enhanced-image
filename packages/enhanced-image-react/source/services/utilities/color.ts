a672950c651422cba58ddfd901b215bfba24e385
Jan Cavel <caveljan@gmail.com>
2020-06-20 11:05:47 +0300

    enhanced-image-react > dev: Text > Textline > TextEditor | color

---
+++ packages/enhanced-image-react/source/services/utilities/color.ts
@@ -0,0 +1,14 @@
+export const resolveColor = (
+    value: string,
+) => {
+    switch (value) {
+        case 'black':
+            return '#000';
+        case 'white':
+            return '#fff';
+        case 'red':
+            return '#f00';
+        default:
+            return value;
+    }
+}
