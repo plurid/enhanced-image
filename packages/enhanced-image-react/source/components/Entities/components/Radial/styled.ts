00e5180a6dbb0da11f18f68d3fdcd39675281c77
Jan Cavel <caveljan@gmail.com>
2020-07-23 09:34:37 +0300

    enhanced-image-react > dev: Entities

--- packages/enhanced-image-react/source/components/Entities/components/Radial/styled.ts
+++ packages/enhanced-image-react/source/components/Entities/components/Radial/styled.ts
@@ -5,4 +5,5 @@ import styled from 'styled-components';
 export const StyledRadial = styled.div`
     position: absolute;
     pointer-events: initial;
+    outline: none;
 `;
--- packages/enhanced-image-react/source/components/Entities/components/Rectangular/styled.ts
+++ packages/enhanced-image-react/source/components/Entities/components/Rectangular/styled.ts
@@ -5,4 +5,5 @@ import styled from 'styled-components';
 export const StyledRectangular = styled.div`
     position: absolute;
     pointer-events: initial;
+    outline: none;
 `;
