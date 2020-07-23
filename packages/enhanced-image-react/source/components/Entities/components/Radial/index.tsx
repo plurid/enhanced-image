21c37ad514d0061259768aec1d6a1b38e4240f90
Jan Cavel <caveljan@gmail.com>
2020-07-23 06:33:44 +0300

    enhanced-image-react > dev: Entities > Radial

--- packages/enhanced-image-react/source/components/Entities/components/Radial/index.tsx
+++ packages/enhanced-image-react/source/components/Entities/components/Radial/index.tsx
@@ -2,6 +2,9 @@
 /** libraries */
 import React, {
     useContext,
+    useRef,
+    useState,
+    useEffect,
 } from 'react';
 
 
@@ -13,6 +16,8 @@ import {
 import {
     Context,
 } from '../../../../services/utilities';
+
+import Editor from '../../../Editor';
 
 
 /** internal */
@@ -62,9 +67,55 @@ const Radial: React.FC<RadialProperties> = (
     const absoluteY = position.y * imageBoxDimensions.height / 100 + 'px';
 
 
+    /** references */
+    const timeoutMouseOver = useRef<any>(0);
+
+
+    /** state */
+    const [showEditor, setShowEditor] = useState(false);
+    const [mouseOver, setMouseOver] = useState(false);
+
+
+    /** handlers */
+    const handleMouseEnter = () => {
+        clearTimeout(timeoutMouseOver.current);
+        setMouseOver(true);
+    }
+
+    const handleMouseLeave = () => {
+        timeoutMouseOver.current = setTimeout(() => {
+            if (mouseOver) {
+                setMouseOver(false)
+            }
+        }, 700);
+    }
+
+
+    /** effects */
+    /**
+     * Handle showEditor
+     */
+    useEffect(() => {
+        if (
+            mouseOver
+            // && editableText
+        ) {
+            setShowEditor(true);
+        } else {
+            setShowEditor(false);
+        }
+    }, [
+        // editableText,
+        mouseOver,
+    ]);
+
+
     /** render */
     return (
         <StyledRadial
+            tabIndex={0}
+            onMouseEnter={() => handleMouseEnter()}
+            onMouseLeave={() => handleMouseLeave()}
             style={{
                 top: absoluteY,
                 left: absoluteX,
@@ -73,7 +124,24 @@ const Radial: React.FC<RadialProperties> = (
                 borderRadius: absoluteRadius / 2 + 'px',
                 backgroundColor: color,
             }}
-        />
+        >
+            {showEditor && (
+                <Editor
+                    positions={{
+                        x: -17,
+                        y: -34,
+                    }}
+                    drawers={[]}
+                    toggleDrawer={() => {}}
+                    setWidth={() => {}}
+                    fullWidth={false}
+                >
+                    <div>
+                        button
+                    </div>
+                </Editor>
+            )}
+        </StyledRadial>
     );
 }
 
