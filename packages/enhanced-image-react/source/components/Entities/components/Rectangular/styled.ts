61accd361809a1ab5e8e0dd950a137a169a40566
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-07-23 17:33:45 +0300

    enhanced-image-react > dev: Entities > Rectangular

--- packages/enhanced-image-react/source/components/Entities/components/Rectangular/styled.ts
+++ packages/enhanced-image-react/source/components/Entities/components/Rectangular/styled.ts
@@ -2,8 +2,36 @@ import styled from 'styled-components';
 
 
 
-export const StyledRectangular = styled.div`
+export interface IStyledRectangular {
+    dragMode: boolean;
+    draggingMode: boolean;
+}
+
+export const StyledRectangular = styled.div<IStyledRectangular>`
     position: absolute;
     pointer-events: initial;
     outline: none;
+
+    cursor: ${
+        ({
+            draggingMode,
+            dragMode,
+            // toggledEditable,
+            // actionable,
+        }: IStyledRectangular) => {
+            // if (actionable && !toggledEditable) {
+            //     return 'pointer';
+            // }
+
+            if (draggingMode) {
+                return 'grabbing';
+            }
+
+            if (dragMode) {
+                return 'grab';
+            }
+
+            return 'initial';
+        }
+    };
 `;
